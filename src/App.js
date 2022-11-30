fdd266e47bcd68f81177f8784decf5b0a90d698c
Roberto Gomez <blanco.roberto020@gmail.com>
2022-11-30 11:55:30 -0300

    images and words

--- src/App.js
+++ src/App.js
@@ -6,28 +6,47 @@ function App() {
 
   const [ conditional, setConditional] = useState(false)
 
+  let palabra;
+
 
 
   useEffect(() =>{ 
 
     const ApiData = async () => {
   
-      /* Comprueba que este en true (desde el form) y si lo está llama la API */
+      /* Traemos la palabra a resolver */
       if( conditional ){
         const url = `https://random-words-api.vercel.app/word/noun`;
   
   
         const respuesta = await fetch(url);
         const resultado = await respuesta.json();
-        console.log(resultado)
-  
+        palabra = resultado[0].word;
+
+          APIPhoto() 
+
       }
     }
   
     ApiData();
   
   }, [conditional]);
+
+
+  /* Traemos las imagenes que describen la palabra */
+    const APIPhoto = async () => {
+
+      if(palabra){
+       
+            const APIKey = `31711953-ed260a8ccebd2c4cd107b6fd4`
+            const urlPhoto = `https://pixabay.com/api/?key=${APIKey}&q=${palabra}&image_type=photo`
+            const respuestaPhoto = await fetch(urlPhoto);
+            const resultadoPhoto = await respuestaPhoto.json();
+            console.log(resultadoPhoto)
+      }
+    }
   
+
   return (
     <div className="font-mono">
 
